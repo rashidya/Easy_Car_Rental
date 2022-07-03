f44fa77ef8016ebd5ca931d997d7e3bdb3a7ec9e
rashidya <rashmiliyanage2000@gmail.com>
2022-07-03 13:09:03 +0530

    Customer entity class created

--- src/main/java/lk/easy/rental/entity/Customer.java
+++ src/main/java/lk/easy/rental/entity/Customer.java
@@ -1,4 +1,29 @@
 package lk.easy.rental.entity;
 
+import lk.easy.rental.embeded.Name;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Customer {
+    @Id
+    private String cusId;
+    private String NIC;
+    @Embedded
+    private Name name;
+    private String drivingLicenseNo;
+    private String address;
+    private String contactNo;
+    private String email;
+
 }
