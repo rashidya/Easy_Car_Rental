3bd2c2b78170e324c78f4ed3a662bf30d468fa1a
rashidya <rashmiliyanage2000@gmail.com>
2022-07-03 13:09:13 +0530

    Driver entity class created

--- src/main/java/lk/easy/rental/entity/Driver.java
+++ src/main/java/lk/easy/rental/entity/Driver.java
@@ -1,4 +1,30 @@
 package lk.easy.rental.entity;
 
+import lk.easy.rental.embeded.Name;
+import lk.easy.rental.enums.Availability;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Driver {
+    @Id
+    private String driverId;
+    private String NIC;
+    @Embedded
+    private Name name;
+    private String drivingLicenseNo;
+    private String address;
+    private String contactNo;
+    private Availability DriverAvailability;
 }
