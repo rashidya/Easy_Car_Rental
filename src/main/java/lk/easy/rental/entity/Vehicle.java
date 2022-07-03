54ba472b67301585b10b3ee0bd42a9a5073d31d5
rashidya <rashmiliyanage2000@gmail.com>
2022-07-03 13:09:55 +0530

    Vehicle entity class created

--- src/main/java/lk/easy/rental/entity/Vehicle.java
+++ src/main/java/lk/easy/rental/entity/Vehicle.java
@@ -1,4 +1,37 @@
 package lk.easy.rental.entity;
 
+import lk.easy.rental.embeded.FreeMileage;
+import lk.easy.rental.embeded.PriceRate;
+import lk.easy.rental.enums.Availability;
+import lk.easy.rental.enums.FuelType;
+import lk.easy.rental.enums.TransmissionType;
+import lk.easy.rental.enums.VehicleType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Vehicle {
+    @Id
+    private String vehicleId;
+    private String registrationNo;
+    private String color;
+    private String brand;
+    private int noOfPassengers;
+    private FuelType fuelType;
+    private VehicleType vehicleType;
+    private TransmissionType transmissionType;
+    private PriceRate pricePerExtraKM;
+    private FreeMileage freeMileage;
+    private Availability vehicleAvailability;
+    private double refundableDamageFee;
+
 }
