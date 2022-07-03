04e00e037f412b5cfd3b1b2207204419220561b0
rashidya <rashmiliyanage2000@gmail.com>
2022-07-03 13:09:33 +0530

    DriverSchedule entity class created

--- src/main/java/lk/easy/rental/entity/DriverSchedule.java
+++ src/main/java/lk/easy/rental/entity/DriverSchedule.java
@@ -1,4 +1,15 @@
 package lk.easy.rental.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class DriverSchedule {
+
+    @Id
+    @GeneratedValue
+    private int driverSchedule_Id;
+
+
 }
