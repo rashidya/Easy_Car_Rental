ed8814c4aff7200a2cedc1dfffdcc43b60c91ca9
rashidya <rashmiliyanage2000@gmail.com>
2022-07-03 13:04:42 +0530

    Configuration Done

--- src/main/java/lk/easy/rental/config/WebAppConfig.java
+++ src/main/java/lk/easy/rental/config/WebAppConfig.java
@@ -1,4 +1,17 @@
 package lk.easy.rental.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = "lk.easy.rental")
+@EnableWebMvc
 public class WebAppConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
