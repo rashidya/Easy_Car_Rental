b8abe9d3a1686538287a9bcc0a7d70c21286df59
rashidya <rashmiliyanage2000@gmail.com>
2022-07-03 13:08:54 +0530

    BookingDetails entity class created

--- src/main/java/lk/easy/rental/entity/BookingDetails.java
+++ src/main/java/lk/easy/rental/entity/BookingDetails.java
@@ -1,4 +1,23 @@
 package lk.easy.rental.entity;
 
+import lk.easy.rental.enums.RequestType;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
 public class BookingDetails {
+    @Id
+    private String booking_Id;
+    private LocalDate pickupDate;
+    private LocalTime pickupTime;
+    private LocalDate returnDate;
+    private LocalTime returnTime;
+    private RequestType needDriver;
+    private double damageFee;
+    private double rentFee;
+
 }
