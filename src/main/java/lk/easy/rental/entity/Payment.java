72eed365def3cdd0f1fd5a1fc110fc8b5efeb39b
rashidya <rashmiliyanage2000@gmail.com>
2022-07-03 13:10:16 +0530

    Payment entity class created

--- src/main/java/lk/easy/rental/entity/Payment.java
+++ src/main/java/lk/easy/rental/entity/Payment.java
@@ -1,4 +1,21 @@
 package lk.easy.rental.entity;
 
-public class Payment {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
+public class Payment{
+    @Id
+    private String paymentID;
+    private LocalDate paymentDate;
 }
