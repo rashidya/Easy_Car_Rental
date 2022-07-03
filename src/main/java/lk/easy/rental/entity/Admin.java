5dff406acdb4fac6dc35cd2ee6af06e4e9447f15
rashidya <rashmiliyanage2000@gmail.com>
2022-07-03 13:08:25 +0530

    Admin class created

--- src/main/java/lk/easy/rental/entity/Admin.java
+++ src/main/java/lk/easy/rental/entity/Admin.java
@@ -1,4 +1,28 @@
 package lk.easy.rental.entity;
 
+import lk.easy.rental.embeded.Name;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Admin{
+    @Id
+    private String cusId;
+    private String NIC;
+    @Embedded
+    private Name name;
+    private String address;
+    private String contactNo;
+    private String email;
+
 }
