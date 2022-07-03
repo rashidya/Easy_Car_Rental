9db00e353fc8fb291746c9a229c334b893691e7c
rashidya <rashmiliyanage2000@gmail.com>
2022-07-03 13:09:45 +0530

    User entity class created

--- src/main/java/lk/easy/rental/entity/User.java
+++ src/main/java/lk/easy/rental/entity/User.java
@@ -1,4 +1,23 @@
 package lk.easy.rental.entity;
 
-public class User {
+import lk.easy.rental.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
+public class User{
+    @Id
+    private String userId;
+    private String userName;
+    private String password;
+    private Role role;
 }
