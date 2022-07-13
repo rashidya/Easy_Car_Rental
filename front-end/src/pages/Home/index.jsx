2666c81ef5afc7af9163dbf78320e705a807db7d
rashidya <rashmiliyanage2000@gmail.com>
2022-07-14 01:10:53 +0530

    Front End Home Page Done

--- front-end/src/pages/Home/index.jsx
+++ front-end/src/pages/Home/index.jsx
@@ -1,35 +1,95 @@
-import {css, Grid} from "@mui/material";
+import {Avatar, Button, Grid, Tab, Tabs, Typography} from "@mui/material";
 import {Component} from "react";
 import {withStyles} from "@mui/styles";
-import Polygon from 'react-polygon';
-import {styleSheet as styles, styleSheet} from "./style";
+import {styleSheet} from "./style";
+import carImage from '../../assets/car.png'
+import * as React from 'react';
+
 
 class Home extends Component {
     myRenderPoint(point) {
-        return <circle cx={point[0]} cy={point[1]} r={5} />;
+        return <circle cx={point[0]} cy={point[1]} r={5}/>;
     }
 
 
-    render () {
-        const {classes}=this.props;
+    render() {
+        const {classes} = this.props;
+
         return (
-            <Grid className={classes.polygon}>
+            <Grid>
 
-                <svg height="100vh" width="60vw">
-                    <polygon
-                        points="1000,0 1000,650 350,650 100,150 250,0"
+                <Grid className={classes.polygon}>
 
-                        fill='purple'
-                        stroke-linejoin="round"
-                        stroke-width="20"
-                        style={{borderRadius:50}}
-                    />
-                </svg>
+                    <Tabs
+                        variant="scrollable"
+                        scrollButtons="auto"
+                        aria-label="scrollable auto tabs example"
+                        style={{position: "absolute", top: "7vh", right: '15vw'}}
+                    >
+                        <Tab label="Home" style={{color: 'white'}}/>
+                        <Tab label="About" style={{color: 'white'}}/>
+                        <Tab label="Contact" style={{color: 'white'}}/>
+                        <Tab label=" Sign In" style={{color: 'white'}}/>
 
-                {/*<Polygon points={'500,10 500,350 300,250 200,70 300,10'} style={{fill:'lime'}} />*/}
-                {/*<Polygon renderPoint={this.myRenderPoint} className={classes.polygon}/>*/}
+                    </Tabs>
+                    <Button variant="outlined" style={{
+                        height: '35px',
+                        color: 'white',
+                        borderColor: 'white',
+                        borderRadius: 50,
+                        position: "absolute",
+                        top: "7.8vh",
+                        right: '8vw'
+                    }}>Sign Up</Button>
+
+
+                    <Avatar src="/broken-image.jpg" style={{position: "absolute", top: "7.5vh", right: '3vw'}}/>
+
+                    <svg height="100vh" width="60vw">
+                        <polygon
+                            points="1000,0 1000,640 430,640 200,150 350,0"
+
+                            fill='purple'
+
+                            style={{stroke: 'purple', strokeWidth: 60, strokeLinejoin: 'round'}}
+                        />
+                    </svg>
+
+                    <img src={carImage}
+                         style={{height: '65vh', position: "absolute", top: '35vh', right: '4vw', display: 'inline'}}/>
+
+                </Grid>
+
+                <Grid>
+                    <Grid>
+                        <Typography variant="h2" component="h2" style={{
+                            color: '#7E7E7E',
+                            position: "absolute",
+                            top: "30vh",
+                            left: '3vw',
+                            fontFamily: 'sans-serif'
+                        }}>
+                            Easy Car Rental
+                        </Typography>
+                        <Typography variant="h5" component="h2"
+                                    style={{color: '#898989', position: "absolute", top: "40vh", left: '3vw'}}>
+                            Hire your Dream Car
+                        </Typography>
+
+                    </Grid>
+
+                    <Grid>
+
+                        <Button variant="contained" href="#contained-buttons"
+                                style={{width:'10vw',position: "absolute", top: "75vh", left: '10vw', backgroundColor: 'black'}}>
+                            Search
+                        </Button>
+                    </Grid>
+
+                </Grid>
 
             </Grid>
+
 
         )
     }
--- front-end/src/pages/Home/style.js
+++ front-end/src/pages/Home/style.js
@@ -2,9 +2,13 @@ export const styleSheet={
     polygon:{
         display:'flex',
         width:'100vw',
-        justifyContent:'flex-end'
+        justifyContent:'flex-end',
+        overflow:'hidden'
     },
 
+    tabs:{
+        color:'white'
+    }
 
 
 }
